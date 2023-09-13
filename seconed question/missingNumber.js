995c7d2c09e1778daa59bdabe23cd5438e313dee
etharsuwan <s11821362@stu.najah.edu>
2023-09-13 04:58:31 +0200

    initial commit

---
+++ seconed question/missingNumber.js
@@ -0,0 +1,9 @@
+function missingNumber(nums) {
+    const n = nums.length;
+    const expectedSum = (n * (n + 1)) / 2; 
+    let actualSum=0;
+for(let i=0 ;i<n;i++) {
+actualSum +=nums[i];
+}   
+    return expectedSum - actualSum;
+  }
